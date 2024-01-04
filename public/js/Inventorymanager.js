6fc94a32492cb4a01df602c37bf57c364da1d680
rajeevk47 <rajeevkumawat2007@gmail.com>
2024-01-04 12:51:11 +0530

    feat : adds feature to vanish projectile from hotbar

--- public/js/Inventorymanager.js
+++ public/js/Inventorymanager.js
@@ -1,18 +1,26 @@
 class inventory{
-    constructor({id={number},image,number}){
+    constructor({id,image,number,Hotbarid}){
         this.id= 25*id
         this.image=image
-        this.id.number
+        this.number=number
+        this.Hotbarid=Hotbarid
 
     }
 
     drawitems(){
-        for(let i=0 ; i <6; i++){
-            c.drawImage(Bulletsforhotbar,this.id*i,0,25,25,35,(80+42*i),25,25)
-        }
+        c.drawImage(Hotbar,10,60,80,400)
         c.lineWidth=8
-    c.strokeStyle='red'
-    c.strokeRect(24,(72+42*mousewheelc),50,40);
-
+        c.strokeStyle='red'
+        c.strokeRect(24,(72+42*mousewheelc),50,40);
+    
+        for(let i=0 ; i <6; i++){
+            if(inv.Hotbarid[i]==0){continue}
+            c.drawImage(Bulletsforhotbar,this.id*i,0,25,25,35,(80+42*i),25,25)
+            c.fillStyle = 'blue'
+            c.fillText(`x${this.Hotbarid[i]}`,80,110+42*i)
+            
+        }
+     
     }
 }
+
