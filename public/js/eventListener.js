861898a80f75cdd915e9307debeec6974a1404ba
rajeevk47 <rajeevkumawat2007@gmail.com>
2024-01-10 23:54:19 +0530

    Modifies number of elements in hotbar

--- public/js/eventListener.js
+++ public/js/eventListener.js
@@ -3,13 +3,13 @@ let mousewheel
 let mousewheelc=0 //mousewheelcount
 addEventListener("wheel", (event) => {
     mousewheel = event.deltaY
-    if(mousewheelc==5&&mousewheel>0){
+    if(mousewheelc==6&&mousewheel>0){
         mousewheelc=0
     }
     else if(mousewheelc==0&& mousewheel<0){
-            mousewheelc=5
+            mousewheelc=6
     }
-    else if(mousewheel>0&&mousewheelc<5){
+    else if(mousewheel>0&&mousewheelc<6){
         mousewheelc+=1
     }else if(mousewheel<0&&mousewheelc>0){
         mousewheelc-=1
@@ -19,7 +19,7 @@ addEventListener("wheel", (event) => {
 
 addEventListener('click', (event) => {
     
-
+                      
     if (!players[socket.id]) { return }
     const playerposition = {
         x: players[socket.id].position.x + players[socket.id].width /2 ,
@@ -32,10 +32,11 @@ addEventListener('click', (event) => {
         x: canvas.width/2+camerax+ (playerdownImage.width/4)/2,
         y:canvas.height/2+cameray+playerdownImage.height/2,
         angle,
-        hotbarid:mousewheelc
+        hotbarid:mousewheelc,
+        playerpos:playerposition
     })
     }else if(event.clientX<66&&event.clientX>24){
-        for(let i = 0;i<6;i++){
+        for(let i = 0;i<7;i++){
             if(event.clientY>80+42*i && event.clientY<80 +43*(i+1)){
                 mousewheelc=i
             }
