82ca65180ed1cc4f46e6746e6301e675c1b7ad23
rajeevk47 <rajeevkumawat2007@gmail.com>
2024-01-19 01:27:53 +0530

    Adjustes projectile size

--- public/js/projectile.js
+++ public/js/projectile.js
@@ -9,7 +9,7 @@ class Projectile {
         this.shootplayerid=shootplayerid
     }
     draw(x,y) {
-        c.drawImage(this.image,this.id,0,15,15,this.position.x-x,this.position.y-y,15,15)
+        c.drawImage(this.image,this.id,0,15,15,this.position.x-x,this.position.y-y,12,12)
         
     }
 
