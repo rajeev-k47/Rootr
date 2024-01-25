0b9364f859fd948f8ab16150f6dfabd529881cc2
rajeevk47 <rajeevkumawat2007@gmail.com>
2024-01-26 03:16:48 +0530

    Feat:Implements the damage of bullets, Bugfix: Score, Feat: Adds random spawing projectiles

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.get('/', (req,res)=>{
 
 const players = {}
 const backendprojectiles = {}
+const spawn ={}
+let i=0
 let projectileID=0
 
 io.on('connection', (socket)=>{
@@ -65,21 +67,20 @@ io.on('connection', (socket)=>{
     })
     
 
-    socket.on('roomchange',(id)=>{
-        players[socket.id].roomid=id
+    socket.on('roomchange',({id,roomid})=>{
+        players[id].roomid=roomid
         io.emit('updatePlayer', players)
-        socket.emit('event',id)
+        socket.emit('event',roomid)
 
     })
-
-    socket.on('shoot', ({ x, y, angle ,hotbarid,playerpos}) => {
+    socket.on('shoot', ({ x, y, angle ,hotbarid,playerpos,shootid}) => {
         projectileID++;
         const velocity = {
-            x: Math.cos(angle) * 2,
-            y: Math.sin(angle) * 2
+            x: Math.cos(angle) * 20,
+            y: Math.sin(angle) * 20
         }
         backendprojectiles[projectileID] = {
-            x, y, velocity, playerID: socket.id,hotbarid,playerpos
+            x, y, velocity, playerID: shootid,hotbarid,playerpos
         }
     })
 
@@ -87,10 +88,12 @@ io.on('connection', (socket)=>{
         delete backendprojectiles[id]
     })
     socket.on('projectilecollisionwp',({id,pid})=>{
-        if(players[id]){players[id].xp-=5}
+        if(players[id]&&backendprojectiles[pid]){
+            players[id].xp-= 2*backendprojectiles[pid].hotbarid+4
         if(players[id].xp<=0){
             delete players[id]
-        }
+            if(backendprojectiles[pid]){players[backendprojectiles[pid].playerID].score+=1}
+        }}
         delete backendprojectiles[pid]
         io.emit('score--ofid',id)
         io.emit('updatePlayer',players)
@@ -104,26 +107,33 @@ io.on('connection', (socket)=>{
         if(id=="heal"&& players[playerid].xp<=215){
             players[playerid].xp+=0.2
         }
-        else if(id=="Resist",players[playerid].res<=215){
+         if(id=="Resist" && players[playerid].res<=215){
             players[playerid].res+=0.3
         }
         io.emit('score--ofid',playerid)
         io.emit('updatePlayer',players)
     })
     socket.on('blastdamage',(id)=>{
-         
-         if(players[id].xp<=0){
+        if(players[id]){
+        players[id].xp-=20
+        if(players[id].xp<=0){
             delete players[id]
         }
-        if(players[id]){players[id].xp-=20}
+        }
           io.emit('score--ofid',id)
           io.emit('updatePlayer',players)
 
 
     })
-    socket.on('shieldhit',(i)=>{
-            players[socket.id].res-=20
+    socket.on('shieldhit',({i,id})=>{
+           if(players[id].res>=0){ players[id].res-=10}
             delete backendprojectiles[i]
+            io.emit('score--ofid',id)
+            io.emit('updatePlayer',players)
+    })
+    socket.on('delspawnitem',(i)=>{
+            delete spawn[i]
+            io.emit('delspawnbybackend',i)
     })
     
     socket.on('disconnect',(reason)=>{
@@ -135,26 +145,17 @@ io.on('connection', (socket)=>{
     
 
 })
-let spawnx,spawny,spawnid
-   
-    setInterval(()=>{
-    spawnx=Math.random()
-    spawny=Math.random()
-    spawnid=Math.random()
-        io.emit('spawnitems',({bspawnx:spawnx,bspawny:spawny,bspawnid:spawnid}))
-    },5000)
+let spawnx,spawny,spawnid,elapsed,currenttime
 setInterval(() => {
-    
-
-
+    currenttime=Date.now()
     for (const id in backendprojectiles) {
         if(backendprojectiles[id].hotbarid==6){
-        backendprojectiles[id].x += backendprojectiles[id].velocity.x/3
-        backendprojectiles[id].y += backendprojectiles[id].velocity.y/3
+        backendprojectiles[id].x += backendprojectiles[id].velocity.x/10
+        backendprojectiles[id].y += backendprojectiles[id].velocity.y/10
         const lastx = backendprojectiles[id].x-backendprojectiles[id].playerpos.x  
         const lasty = backendprojectiles[id].y-backendprojectiles[id].playerpos.y 
+        
         if(Math.sqrt(lastx*lastx + lasty*lasty)>200){
-          
           io.emit('blast',({lastx:backendprojectiles[id].x,lasty:backendprojectiles[id].y}))
           delete backendprojectiles[id]
        }
@@ -166,9 +167,29 @@ setInterval(() => {
         }
     }
     io.emit('updateprojectiles', backendprojectiles)
-    // io.emit('updatePlayer',players)
 },15)
 
-server.listen(port, ()=> {
+setInterval(()=>{
+    spawnx=Math.random()
+    spawny=Math.random()
+    spawnid=Math.random()
+    elapsed=Date.now()
+    spawn[i] = {
+        position:{x:spawnx*1380+115,y:spawny*650+70},
+        id:parseInt(spawnid*7),
+        roomid:parseInt(Math.random()*3-1),//prob of 0 is greater
+        number:i,
+        elapsed:elapsed
+    }
+    for(const j in spawn){if(spawn[j].elapsed+16000<currenttime){
+        delete spawn[j]
+        io.emit('delspawnbybackend',j)}}
+    i++
+    io.emit('spawnitems',spawn)
+},5000)
+
+
+
+server.listen(port,'10.81.29.30', ()=> {
     console.log(`App is listening on ${port}`)
 } )
